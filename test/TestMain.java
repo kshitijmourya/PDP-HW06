fcea19353fcba29b220d9f3901148544429c158b
kshitijmourya <mourya.k@husky.neu.edu>
2018-10-31 01:58:55 -0400

    junit tests

--- test/TestMain.java
+++ test/TestMain.java
@@ -12,7 +12,7 @@ public class TestMain {
   public static void main(String[] args) {
     helper(FreecellModel
             .getBuilder()
-            .cascades(6)
+            .cascades(52)
             .opens(4)
             .build());
 
@@ -25,84 +25,11 @@ public class TestMain {
     //System.out.print(model.getGameState());
     //System.out.println();
     //System.out.println("**********************************************************");
-
     //System.out.println("**********************************************************");
 
     // Moves to finish the game.
-
-/*
-
-    model.move(PileType.CASCADE, 0, 13, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 1, 13, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 2, 13, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 3, 13, PileType.FOUNDATION, 3);
-    System.out.println(model.getGameState());
-    model.move(PileType.CASCADE, 4, 12, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 5, 12, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 6, 12, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 7, 12, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 0, 11, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 1, 11, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 2, 11, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 3, 11, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 4, 10, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 5, 10, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 6, 10, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 7, 10, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 0, 9, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 1, 9, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 2, 9, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 3, 9, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 4, 8, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 5, 8, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 6, 8, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 7, 8, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 0, 7, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 1, 7, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 2, 7, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 3, 7, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 4, 6, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 5, 6, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 6, 6, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 7, 6, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 0, 5, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 1, 5, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 2, 5, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 3, 5, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 4, 4, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 5, 4, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 6, 4, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 7, 4, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 0, 3, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 1, 3, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 2, 3, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 3, 3, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 4, 2, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 5, 2, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 6, 2, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 7, 2, PileType.FOUNDATION, 3);
-
-    model.move(PileType.CASCADE, 0, 1, PileType.FOUNDATION, 0);
-    model.move(PileType.CASCADE, 1, 1, PileType.FOUNDATION, 1);
-    model.move(PileType.CASCADE, 2, 1, PileType.FOUNDATION, 2);
-    model.move(PileType.CASCADE, 3, 1, PileType.FOUNDATION, 3);
-
-
-    System.out.println(model.getGameState());
-*/
-
-/* Cascade 52 open 4
-model.move(PileType.CASCADE,0,1,PileType.FOUNDATION,0);
+    /* Cascade 52 open 4
+    model.move(PileType.CASCADE,0,1,PileType.FOUNDATION,0);
     model.move(PileType.CASCADE,1,1,PileType.FOUNDATION,1);
     model.move(PileType.CASCADE,2,1,PileType.FOUNDATION,2);
     model.move(PileType.CASCADE,3,1,PileType.FOUNDATION,3);
@@ -183,11 +110,11 @@ model.move(PileType.CASCADE,0,1,PileType.FOUNDATION,0);
     //model.startGame(deck,true);
 
 
-    model.move(PileType.CASCADE,5,13,PileType.FOUNDATION,0);
+    //model.move(PileType.CASCADE,5,13,PileType.FOUNDATION,0);
 
     //System.out.println(model.getGameState());
     //model.move(PileType.CASCADE,2,13,PileType.FOUNDATION,0);
-    System.out.println(model.getGameState());
+    //System.out.println(model.getGameState());
 
 
     System.out.println(model.isGameOver());
